72b5bf8a04d2ecba0c4dbea2add8e2dcdad9b9ab
Brian Whitton <me@brianwhitton.com>
2018-06-08 16:53:57 -0400

    update chapter model key names

--- app/models/chapter.js
+++ app/models/chapter.js
@@ -5,11 +5,14 @@ export default DS.Model.extend({
   type: DS.attr('string'),
   title: DS.attr('string'),
   slug: DS.attr('string'),
-  videoId: DS.attr('string'),
-  duration: DS.attr('string'),
   content: DS.attr('string'),
   links: DS.attr(),
   attachments: DS.attr(),
+
+  youtubeId: DS.attr('string'),
+  youtubeThumbnail: DS.attr('string'),
+  duration: DS.attr('string'),
+  
   typeformEmbed: DS.attr('string'),
 
   week: DS.belongsTo('week'),
