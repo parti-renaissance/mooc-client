422cccc00786f708dfd526dc89774e0382745cde
Brian Whitton <me@brianwhitton.com>
2018-06-08 16:58:58 -0400

    chapter list test

--- tests/integration/components/chapter-list/component-test.js
+++ tests/integration/components/chapter-list/component-test.js
@@ -1,17 +1,16 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find } from '@ember/test-helpers';
+import { render, find, findAll } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | chapter-list', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{chapter-list}}`);
+    this.set('chapters', [1, 2, 3])
+    await render(hbs`{{chapter-list chapters}}`);
 
     assert.ok(find('.chapter-list'));
+    assert.equal(findAll('.chapter-item').length, 3);
   });
 });
