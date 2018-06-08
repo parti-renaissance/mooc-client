19349e67aa13816b3712ee5f8bd6e0648f5c73d5
Brian Whitton <me@brianwhitton.com>
2018-06-08 16:53:45 -0400

    updates fixture with live data

--- mirage/fixtures/mooc.js
+++ mirage/fixtures/mooc.js
@@ -1,147 +1,66 @@
 export default {
-  "title": "MOOC Title",
-  "slug": "mooc-title",
+  "title": "Faire de sa fourchette un acte politique",
+  "slug": "faire-de-sa-fourchette-un-acte-politique",
   "elements": [
     {
-      "type": "chapter",
-      "title": "Week 1",
-      "slug": "week-1",
-      "description": "My week 1",
-      "publishedAt": "2025-06-12T09:30:00+02:00"
+     "type": "chapter",
+      "title": "Semaine 1 : Le coup de fourchette pour détendre notre santé",
+      "slug": "semaine-1-le-coup-de-fourchette-pour-detendre-notre-sante",
+      "publishedAt": "2018-06-09 09:30:00"
     },
     {
       "type": "video",
-      "title": "Article Name One",
-      "slug": "article-name-one",
-      "videoId": "M7lc1UVf-VE",
-      "duration": "3:39",
-      "content": "<div><p>Lorem ipsum dolor sit amet, consectetur adipisicing</p></div>",
-      "links": [
-        {
-          "linkName": "My first link",
-          "linkUrl": "my_link_url.googlestorage.com"
-        },
-        {
-          "linkName": "My second link",
-          "linkUrl": "my_second_link_url.googlestorage.com"
-        }
-      ],
-      "attachments": [
-        {
-          "attachmentName": "My first attachment",
-          "attachmentUrl": "my_attachment.googlestorage.com"
-        },
-        {
-          "attachmentName": "My second attachment",
-          "attachmentUrl": "my_second_attachment.googlestorage.com"
-        },
-        {
-          "attachmentName": "My second attachment",
-          "attachmentUrl": "my_second_attachment.googlestorage.com"
-        }
-      ]
+      "title": "Les produits transformés dans une première vidéo",
+      "slug": "les-produits-transformes-dans-une-premiere-video",
+      "content": "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>",
+      "links": [{
+        "linkName": "Les sites départementaux de La République En Marche",
+        "linkUrl": "http://dpt.en-marche.fr"
+      }],
+      "attachments": [{
+        "attachmentName": "Bonsoir GitHub",
+        "attachmentUrl": "http://enmarche.dev:8000/mooc/file/af916a45-8cd2-4557-a8b8-83d2ebeb258c"
+      }],
+      "youtubeId": "ktHEfEDhscU",
+      "youtubeThumbnail": "https://img.youtube.com/vi/ktHEfEDhscU/0.jpg",
+      "duration": "00:02:10"
     },
     {
       "type": "video",
-      "title": "Article Name Two",
-      "slug": "article-name-two",
-      "videoId": "M7lc1UVf-VE",
-      "duration": "3:39",
-      "content": "<div><p>Lorem ipsum dolor sit amet, consectetur adipisicing</p></div>",
-      "links": [
-        {
-          "linkName": "My first link",
-          "linkUrl": "my_link_url.googlestorage.com"
-        },
-        {
-          "linkName": "My second link",
-          "linkUrl": "my_second_link_url.googlestorage.com"
-        }
-      ],
-      "attachments": [
-        {
-          "attachmentName": "My first attachment",
-          "attachmentUrl": "my_attachment.googlestorage.com"
-        },
-        {
-          "attachmentName": "My second attachment",
-          "attachmentUrl": "my_second_attachment.googlestorage.com"
-        },
-        {
-          "attachmentName": "My second attachment",
-          "attachmentUrl": "my_second_attachment.googlestorage.com"
-        }
-      ]
+      "title": "Les produits transformés dans une deuxième vidéo",
+      "slug": "les-produits-transformes-dans-une-deuxieme-video",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.",
+      "links": [],
+      "attachments": [],
+      "youtubeId": "ktHEfEDhscU",
+      "youtubeThumbnail": "https://img.youtube.com/vi/ktHEfEDhscU/0.jpg",
+      "duration": "01:30:00"
     },
     {
-      "type": "video",
-      "title": "Article Name Three",
-      "slug": "article-name-three",
-      "videoId": "M7lc1UVf-VE",
-      "duration": "3:39",
-      "content": "<div><p>Lorem ipsum dolor sit amet, consectetur adipisicing</p></div>",
-      "links": [
-        {
-          "linkName": "My first link",
-          "linkUrl": "my_link_url.googlestorage.com"
-        },
-        {
-          "linkName": "My second link",
-          "linkUrl": "my_second_link_url.googlestorage.com"
-        }
-      ],
-      "attachments": [
-        {
-          "attachmentName": "My first attachment",
-          "attachmentUrl": "my_attachment.googlestorage.com"
-        },
-        {
-          "attachmentName": "My second attachment",
-          "attachmentUrl": "my_second_attachment.googlestorage.com"
-        },
-        {
-          "attachmentName": "My second attachment",
-          "attachmentUrl": "my_second_attachment.googlestorage.com"
-        }
-      ]
+      "type": "quizz",
+      "title": "Le test de votre vie",
+      "slug": "le-test-de-votre-vie",
+      "content": "<p>une description</p>",
+      "links": [],
+      "attachments": [],
+      "typeformEmbed": "<p>le quizz</p>"
     },
     {
       "type": "chapter",
-      "title": "Week 2",
-      "slug": "week-2",
-      "description": "My week 2",
-      "publishedAt": "2025-06-12T09:30:00+02:00"
+      "title": "Semaine 2 : Le coup de fourchette pour défendre la nature",
+      "slug": "semaine-2-le-coup-de-fourchette-pour-defendre-la-nature",
+      "publishedAt": "2018-06-13 09:30:00"
     },
     {
-      "type": "quiz",
-      "title": "My quiz Name",
-      "slug": "my-quiz-name",
-      "typeformEmbed": "<iframe src='typefrom' frameborder='0'></iframe>",
-      "content": "<div> <p>Lorem ipsum dolor sit amet, consectetur</div>",
-      "links": [
-        {
-          "linkName": "My first link",
-          "linkUrl": "my_link_url.googlestorage.com"
-        },
-        {
-          "linkName": "My second link",
-          "linkUrl": "my_second_link_url.googlestorage.com"
-        }
-      ],
-      "attachments": [
-        {
-          "attachmentName": "My first attachment",
-          "attachmentUrl": "my_attachment.googlestorage.com"
-        },
-        {
-          "attachmentName": "My second attachment",
-          "attachmentUrl": "my_second_attachment.googlestorage.com"
-        },
-        {
-          "attachmentName": "My second attachment",
-          "attachmentUrl": "my_second_attachment.googlestorage.com"
-        }
-      ]
+      "type": "video",
+      "title": "Les produits transformés dans une troisième vidéo",
+      "slug": "les-produits-transformes-dans-une-troisieme-video",
+      "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "links": [],
+      "attachments": [],
+      "youtubeId": "ktHEfEDhscU",
+      "youtubeThumbnail": "https://img.youtube.com/vi/ktHEfEDhscU/0.jpg",
+      "duration": "00:30:15"
     }
   ]
 };
