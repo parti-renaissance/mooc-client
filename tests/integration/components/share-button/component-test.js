3ed728693df57749a4411ab8e157c9da0b27a856
Brian Whitton <me@brianwhitton.com>
2018-06-09 11:07:51 -0400

    share button tests

--- tests/integration/components/share-button/component-test.js
+++ tests/integration/components/share-button/component-test.js
@@ -1,17 +1,15 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find } from '@ember/test-helpers';
+import { render, find, findAll } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | share-button', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`{{share-button}}`);
 
     assert.ok(find('.share-button'));
+    assert.equal(findAll('.share-button__share').length, 3, 'all share buttons render');
   });
 });
--- tests/integration/components/share-button/share/component-test.js
+++ tests/integration/components/share-button/share/component-test.js
@@ -1,26 +1,46 @@
-import { module, test } from 'qunit';
+import { module } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import test from 'ember-sinon-qunit/test-support/test';
 
 module('Integration | Component | share-button/share', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`{{share-button/share}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#share-button/share}}
-        template block text
-      {{/share-button/share}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.ok(find('.share-button__share'));
   });
+
+  test('facebook', async function() {
+    let newWindow = {focus() {}};
+    this.mock(newWindow).expects('focus');
+    this.mock(window)
+      .expects('open')
+      .withArgs(`https://www.facebook.com/sharer.php?u=${window.location}`, 'share window')
+      .returns(newWindow);
+
+    await render(hbs`{{share-button/share 'facebook'}}`);
+    await click('.share-button__share');
+  });
+
+  test('twitter', async function() {
+    let newWindow = {focus() {}};
+    this.mock(newWindow).expects('focus');
+    this.mock(window)
+      .expects('open')
+      .withArgs(`https://twitter.com/intent/tweet?text=foo&url=${window.location}`, 'share window')
+      .returns(newWindow);
+
+    await render(hbs`{{share-button/share 'twitter' params=(hash text='foo')}}`);
+    await click('.share-button__share');
+  });
+
+  test('email', async function(assert) {
+    await render(hbs`{{share-button/share 'email' params=(hash text='whatever')}}`);
+
+    assert.ok(find('.share-button__share'));
+    assert.equal(find('.share-button__share.email').getAttribute('href'), 'mailto:?subject=whatever');
+  })
 });
