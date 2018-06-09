bac3f2f37f2bbd01fee46a491d88d85442f3cb88
Brian Whitton <me@brianwhitton.com>
2018-06-09 11:27:16 -0400

    implement next/prev logic

--- app/components/chapter-ui/component.js
+++ app/components/chapter-ui/component.js
@@ -1,6 +1,17 @@
 import Component from '@ember/component';
+import { inject } from '@ember/service';
 
 export default Component.extend({
   tagName: 'article',
-  classNames: ['chapter-ui']
+  classNames: ['chapter-ui'],
+
+  router: inject(),
+
+  next() {
+    this.router.transitionTo('mooc.chapter', this.chapter.nextChapter.slug);
+  },
+
+  previous() {
+    this.router.transitionTo('mooc.chapter', this.chapter.previousChapter.slug);
+  }
 });
