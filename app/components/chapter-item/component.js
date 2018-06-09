197106e90ef4b1bf6a7e2bd16e1f23fdf32c2b61
Brian Whitton <me@brianwhitton.com>
2018-06-09 11:07:39 -0400

    chapter item tests

--- app/components/chapter-item/component.js
+++ app/components/chapter-item/component.js
@@ -1,7 +1,31 @@
 import Component from '@ember/component';
+import { computed, get } from '@ember/object';
+import moment from 'moment';
+import { htmlSafe } from '@ember/string';
 
 const Item = Component.extend({
-  tagName: ''
+  tagName: '',
+
+  parsedYtThumb: computed('chapter.youtubeThumbnail', function() {
+    return htmlSafe(`background-image: url(${this.chapter.youtubeThumbnail})`);
+  }),
+
+  parsedDuration: computed('chapter.duration', function() {
+    let duration = moment.duration(get(this, 'chapter.duration'));
+    let [hours, minutes, seconds] = [duration.hours(), duration.minutes(), duration.seconds()];
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    if (hours > 0) {
+      return `${hours}:${minutes}:${seconds}`;
+    } else {
+      return `${minutes}:${seconds}`;
+    }
+  })
 });
 
 Item.reopenClass({
--- tests/integration/components/chapter-item/component-test.js
+++ tests/integration/components/chapter-item/component-test.js
@@ -2,19 +2,34 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { set } from '@ember/object';
 
 module('Integration | Component | chapter-item', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    this.set('chapter', {
+    let chapter = {
       title: 'Foo',
-      duration: '3:00'
-    })
+      duration: '00:30:15',
+      youtubeThumbnail: 'https://img.youtube.com/vi/ktHEfEDhscU/0.jpg',
+      type: 'video'
+    };
+
+    this.set('chapter', chapter);
     await render(hbs`{{chapter-item chapter}}`);
 
     assert.ok(find('.chapter-item'));
-    assert.equal(find('.chapter-item__title').textContent.trim(), 'Foo');
-    assert.equal(find('.chapter-item__duration').textContent.trim(), '3:00');
+    assert.equal(find('.chapter-item__title').textContent.trim(), 'Foo', 'title');
+    assert.equal(find('.chapter-item__duration').textContent.trim(), '30:15', 'duration');
+    assert.equal(find('.chapter-item__yt-thumb').getAttribute('style'), `background-image: url(${chapter.youtubeThumbnail});`, 'thumbnail');
+
+    set(chapter, 'duration', '01:02:01');
+    await render(hbs`{{chapter-item chapter}}`);
+    assert.equal(find('.chapter-item__duration').textContent.trim(), '1:02:01', 'duration');
+
+    delete chapter.youtubeThumbnail;
+    set(chapter, 'type', 'quiz');
+    await render(hbs`{{chapter-item chapter}}`);
+    assert.ok(find('.chapter-item__quiz-thumb .fa-check'), 'renders checkmark icon');
   });
 });
