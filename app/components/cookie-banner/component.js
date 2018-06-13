5a1508409964102671ad73a419088b43bda84afb
Brian Whitton <me@brianwhitton.com>
2018-06-12 23:32:01 -0400

    adds cookie banner

---
+++ app/components/cookie-banner/component.js
@@ -0,0 +1,23 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  tagName: '',
+  shouldOpen: false,
+
+  init() {
+    this._super(...arguments);
+    if (typeof document !== 'undefined' && !document.cookie.match('cookieconsent_status')) {
+      this.set('shouldOpen', true);
+    }
+  },
+
+
+  actions: {
+    dismiss() {
+      this.set('shouldOpen', false);
+      let date = new Date();
+      date.setFullYear(date.getFullYear() + 1);
+      document.cookie = `cookieconsent_status=dismissed; expires=${date.toString()}`
+    }
+  }
+});
---
+++ tests/integration/components/cookie-banner/component-test.js
@@ -0,0 +1,32 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, click } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | cookie-banner', function(hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(function() {
+    document.cookie = 'cookieconsent_status=; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+  });
+
+  test('it renders', async function(assert) {
+    await render(hbs`{{cookie-banner}}`);
+
+    assert.ok(this.element.querySelector('.cookie-banner'), 'renders');
+  });
+
+  test('it does not render if the cookieconsent_status is set to dismissed', async function(assert) {
+    document.cookie = 'cookieconsent_status=dismissed;';
+
+    await render(hbs`{{cookie-banner}}`);
+    assert.notOk(this.element.querySelector('.cookie-banner'), 'should be closed if cookie is set');
+  })
+
+  test('dismissing should set the cookie', async function(assert) {
+    await render(hbs`{{cookie-banner}}`);
+    await click('[data-test-selector="cookie-button"]');
+
+    assert.ok(document.cookie.match('cookieconsent_status'), 'cookie should be set');
+  });
+});
