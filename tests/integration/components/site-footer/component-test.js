4e8296324e2d02e88a91b1ec7777ab680ec7cdfa
Brian Whitton <me@brianwhitton.com>
2018-06-12 23:00:45 -0400

    adds a site footer

---
+++ tests/integration/components/site-footer/component-test.js
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, find } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | site-footer', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    await render(hbs`{{site-footer}}`);
+
+    assert.ok(find('.site-footer'));
+  });
+});
