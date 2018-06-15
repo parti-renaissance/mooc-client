8eb2012199a6b358c33e37f54dd63b92f5fc66d6
Brian Whitton <me@brianwhitton.com>
2018-06-15 17:43:16 -0400

    add new fields to mooc model

--- app/models/mooc.js
+++ app/models/mooc.js
@@ -3,6 +3,12 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   slug: DS.attr('string'),
+  content: DS.attr('string'),
+  youtubeDuration: DS.attr('string'),
+  youtubeId: DS.attr('string'),
+  youtubeThumbnail: DS.attr('string'),
+
+  type: DS.attr('string', {defaultValue: 'mooc'}),
 
   weeks: DS.hasMany('week')
 });
