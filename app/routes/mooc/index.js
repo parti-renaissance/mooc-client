103fe62986c52e325ba5f71a04f111e473893663
Brian Whitton <me@brianwhitton.com>
2018-06-15 17:43:03 -0400

    return the mooc itself instead of redirecting for the index

--- app/routes/mooc/index.js
+++ app/routes/mooc/index.js
@@ -1,10 +1,24 @@
 import Route from '@ember/routing/route';
+import { inject } from '@ember/service';
+import { reads } from '@ember/object/computed';
 
 export default Route.extend({
-  redirect() {
-    let mooc = this.modelFor('mooc');
-    let firstWeek = mooc.get('weeks.firstObject');
-    let firstChapter = firstWeek.get('chapters.firstObject');
-    this.transitionTo('mooc.chapter', firstChapter.get('slug'));
+  head: inject('head-data'),
+  fastboot: inject(),
+  isFastBoot: reads('fastboot.isFastBoot'),
+
+  titleToken: model => model.get('title'),
+
+  model() {
+    return this.modelFor('mooc');
+  },
+
+  afterModel(model) {
+    this._super(...arguments);
+
+    this.get('head').setProperties({
+      chapterTitle: model.get('title'),
+      image: model.get('youtubeThumbnail')
+    });
   }
 });
