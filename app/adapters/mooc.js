73f32866ebdb9a68531a094d2666d3b24adfd974
Brian Whitton <me@brianwhitton.com>
2018-09-16 12:03:15 -0400

    add findall method to mooc adapter

--- app/adapters/mooc.js
+++ app/adapters/mooc.js
@@ -3,6 +3,9 @@ import fetch from 'fetch';
 import config from '../config/environment';
 
 export default DS.Adapter.extend({
+  findAll() {
+    return fetch(config.moocAPI).then(r => r.json());
+  },
   findRecord(store, type, slug) {
     return fetch(`${config.moocAPI}/${slug}`).then(r => r.json());
   }
