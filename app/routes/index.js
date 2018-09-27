cdf9bcf333597246af1fdd2064c7b854b0e83abb
Brian Whitton <me@brianwhitton.com>
2018-09-26 20:53:44 -0400

    add canonical url

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,13 @@
 import Route from '@ember/routing/route';
 import { inject } from '@ember/service';
+import { reads } from '@ember/object/computed';
 
 const META_DESCRIPTION = "Découvrez les MOOC de La République En Marche ! Entièrement gratuits, les MOOC (ou Massive Open Online Course) sont des cours accessibles en ligne créés par LaREM à destination de tous ceux qui veulent approfondir leurs compétences citoyennes.";
 
 export default Route.extend({
   head: inject('head-data'),
+  fastboot: inject(),
+  isFastBoot: reads('fastboot.isFastBoot'),
 
   title() {
     return 'MOOC | La République En Marche !';
@@ -20,6 +23,11 @@ export default Route.extend({
       chapterTitle: "MOOC de La République En Marche !",
       image: 'https://storage.googleapis.com/en-marche-fr/E-MAILING/2017/images/REM/Logo-LREM-noir.jpg'
     });
+
+    if (this.isFastBoot) {
+      let { protocol, host, path } = this.fastboot.request;
+      this.set('url', `${protocol}//${host}${path}`);
+    }
   },
 
 
